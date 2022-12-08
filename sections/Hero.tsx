ab218e852f49c83c71015b2810189c525ad1bfb9
John Canero <johncanero.official@gmail.com>
2022-12-08 20:51:59 +0800

    Adjust Mobile Responsive

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -4,10 +4,10 @@ import { GiPerspectiveDiceSixFacesFour } from "@react-icons/all-files/gi/GiPersp
 
 const Hero = () => {
   return (
-    <div className="mt-12 text-center md:mx-32 lg:mx-96 font-Roboto">
+    <div className="mx-2 mt-12 text-center md:mx-32 lg:mx-96 font-Roboto">
       <div>
         {/* Card */}
-        <div className="px-12 py-10 mx-auto border border-orange-400 rounded-lg dark:border-none bg-neutral-200 md:max-w-xl shadow-20 dark:bg-zinc-800">
+        <div className="py-10 mx-auto border border-orange-400 rounded-lg md:px-12 dark:border-none bg-neutral-200 md:max-w-xl shadow-20 dark:bg-zinc-800">
           <p className="text-sm font-bold tracking-widest text-orange-400 ">
             ADVICE # 1
           </p>
@@ -19,14 +19,14 @@ const Hero = () => {
 
           {/* Horizontal Line */}
           <div className="flex mt-8">
-            <hr className="h-px mx-auto mt-4 bg-gray-700 border-0 md:w-32 lg:w-40 dark:bg-neutral-700" />
+            <hr className="h-px mx-auto mt-4 bg-gray-700 border-0 w-28 md:w-32 lg:w-40 dark:bg-neutral-700" />
             <p className="text-3xl">;;</p>
-            <hr className="h-px mx-auto mt-4 bg-gray-700 border-0 md:w-32 lg:w-40 dark:bg-neutral-700" />
+            <hr className="h-px mx-auto mt-4 bg-gray-700 border-0 w-28 md:w-32 lg:w-40 dark:bg-neutral-700" />
           </div>
         </div>
         {/* Button */}
         <div>
-          <button className="relative z-50 px-2 py-2 mt-5 font-bold text-black bg-orange-400 rounded hover:bg-orange-600">
+          <button className="relative z-50 px-2 py-2 mt-5 font-bold text-black bg-orange-400 rounded hover:bg-orange-500">
             <GiPerspectiveDiceSixFacesFour size={48} />
           </button>
         </div>
